8410e926a1bdd79e7ccfe305257aa8e1267262e3
yaomm <yaomm93@163.com>
2019-09-14 11:57:39 +0800

    User_Hello

---
+++ src/com/powerlbs/User.java
@@ -0,0 +1,14 @@
+package com.powerlbs;
+
+/**
+ * ClassName:${name}
+ * package:com.powerlbs
+ *
+ * @date:2019/9/14 11:41
+ * @author:Yaoxiaoming
+ **/
+public class User {
+    public static void main(String[] args) {
+        System.out.println("你好");
+    }
+}
